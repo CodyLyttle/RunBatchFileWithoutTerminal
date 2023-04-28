f0be800177445894c2beb89d800320053a25fab7
Cody Lyttle <codylyttle123@gmail.com>
2023-04-28 14:58:54 +1200

    Add project.

---
+++ RunBatchFileWithoutTerminal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace RunBatchFileWithoutTerminal
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length is 0)
+            {
+                DisplayErrorMessage("Missing Argument: Path to target batch file.");
+                return;
+            }
+
+            string batchFilePath = args[0];
+
+            if (!File.Exists(batchFilePath))
+            {
+                DisplayErrorMessage($"File '{batchFilePath}' does not exist.");
+                return;
+            }
+
+            if (Path.GetExtension(batchFilePath).ToLower() != ".bat")
+            {
+                DisplayErrorMessage($"File '{batchFilePath}' is not a batch file.");
+                return;
+            }
+
+            ProcessStartInfo startInfo = new()
+            {
+                FileName = batchFilePath,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            using Process? batchProcess = Process.Start(startInfo);
+            batchProcess?.Dispose();
+        }
+
+        private static void DisplayErrorMessage(string message)
+        {
+            MessageBox.Show(message, "RunBatchFileWithoutTerminal", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
